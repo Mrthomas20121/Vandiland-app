444fc5313f791e6f88bc18ef6380ea3a9f0696db
Thomas Blavet <thomas77300@gmail.com>
2018-07-20 09:55:26 +0200

    Database change

--- database.sql
+++ database.sql
@@ -1,19 +1,18 @@
 
-CREATE TABLE punishments(
-  userID varchar(100) NOT NULL,
-  type varchar(10) NOT NULL
-  moderator varchar(30) NOT NULL
-  reason varchar(255) NOT NULL,
-  time varchar(100),
-  channel varchar(255) NOT NULL,
-  PRIMARY KEY(userID)
-);
 CREATE TABLE users(
-  id varchar(40) NOT NULL,
+  id varchar(100) NOT NULL,
   user varchar(255) NOT NULL,
-  mod boolean NOT NULL
-  PRIMARY KEY(userID)
+  `mod` varchar(5) NOT NULL,
+  PRIMARY KEY(id)
 );
-
-ALTER TABLE punishments ADD CONSTRAINT fk_punishment_id FOREIGN KEY (userid) REFERENCES users(id);
-ALTER TABLE punishments ADD CONSTRAINT fk_mod_punishment FOREIGN KEY (moderator) REFERENCES users(mod);
+CREATE TABLE punishments(
+  id INT AUTO_INCREMENT NOT NULL,
+  userID varchar(100) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  moderator varchar(30) NOT NULL,
+  reason varchar(255) NOT NULL,
+  `time` varchar(100),
+  channel varchar(255) NOT NULL,
+  PRIMARY KEY(id)
+);
+ALTER TABLE punishments ADD CONSTRAINT fk_users_id FOREIGN KEY (userID) REFERENCES users(id);
