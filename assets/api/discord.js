04d68db55bd723cf7ea68ca5c93c6c5e4391356b
mrthomas20121 <thomas77300@gmail.com>
2020-04-25 20:05:37 +0200

    Added Nick and Removed unused stuff

--- assets/api/discord.js
+++ assets/api/discord.js
@@ -19,12 +19,11 @@ client.on('ready', (event)=> {
   client.getAllUsers();
 
 });
-client.on("allUsers", function() { 
+client.on("allUsers", function(event) { 
   console.log("done");
   
-  let serverID = "333341155737731072";
-  // let serverID = "358344470749511680";
-  // let serverID = bot.channels[channelID].guild_id
+  let serverID = require('../auth.json').server_id;
+  //let serverID = client.channels[channelID].guild_id
   let server = client.servers[serverID];
   let members = server.members;
   //console.log(members);
@@ -48,63 +47,34 @@ client.on("allUsers", function() {
       cards.appendChild(card);
       let sub = document.createElement('span');
       sub.classList.add('sub');
-      sub.innerHTML = `${client.users[member.id].username}#${client.users[member.id].discriminator} &middot; UserID:  ${member.id}`;
+      if(member.hasOwnProperty('nick')) {
+        sub.innerHTML = `${client.users[member.id].username}#${client.users[member.id].discriminator} &middot; UserID:  ${member.id} &middot; Nickname: ${member.nick}`;
+      } 
+      else {
+        sub.innerHTML = `${client.users[member.id].username}#${client.users[member.id].discriminator} &middot; UserID:  ${member.id} &middot; No Nickname found.`;
+      }
       card.appendChild(sub);
       let ul = document.createElement('ul');
       ul.id = "roles"
       ul.classList.add('rolelist');
       let r = [];
+      if(userRoles.length == 0) {
+        let l = document.createElement('li');
+        l.innerHTML = 'No roles'
+        l.classList.add('nostyle')
+        ul.appendChild(l)
+      }
       userRoles.forEach(element => {
         let name = Object.values(client.servers[serverID].roles).find(r => r.id === element);
         let li = document.createElement('li');
         li.style.backgroundColor=`#${name.color.toString(16)}`;
-        if(name.name == "Joestars") {
+        if(name.color.toString(16).toLowerCase() == "ffffff") {
           li.style.color="#000";
         }
         li.innerHTML = name.name
         ul.appendChild(li);
       });
       card.appendChild(ul);
-      let modpanel = document.createElement('div');
-      modpanel.classList.add('modpanel');
-
-      let ban = document.createElement('div');
-      ban.classList.add('noselect');
-      ban.innerHTML = "ban";
-      ban.addEventListener("click", () => {
-        location = `query.html?q=${client.users[member.id].username}&p=ban`
-      });
-      modpanel.appendChild(ban);
-
-      let kick = document.createElement('div');
-      kick.classList.add('noselect');
-      kick.innerHTML = "kick";
-      kick.addEventListener("click", () => {
-        location = `query.html?q=${member.id}&p=kick`
-      });
-      modpanel.appendChild(kick);
-
-      let mute = document.createElement('div');
-      mute.classList.add('noselect');
-      mute.innerHTML = "mute";
-      mute.addEventListener("click", () => {
-        location = `query.html?q=${member.id}&p=mute`
-      });
-      modpanel.appendChild(mute);
-
-      let warns = document.createElement('div');
-      warns.classList.add('noselect');
-      warns.innerHTML = "warn";
-      warns.addEventListener("click", () => {
-        location = `query.html?q=${member.id}&p=warn`
-      });
-      modpanel.appendChild(warns);
-      card.addEventListener("mouseover", () => {
-        card.appendChild(modpanel);
-      });
-      card.addEventListener("mouseleave", (element, ev) => {
-        card.removeChild(modpanel);
-      });
     }
   }
   client.disconnect();
